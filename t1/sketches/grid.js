d81739e266ae54e066cdb733f91a4e5c882acc74
jdpp95 <jdporrasp@unal.edu.co>
2018-09-08 22:46:17 -0500

    Grid optical illusion

---
+++ t1/sketches/grid.js
@@ -0,0 +1,27 @@
+function setup(){
+	createCanvas(600,600);
+	background(0);
+}
+
+function draw(){
+	var nLines = 20;
+	var thickness = 8;
+	//stroke(128);
+	//strokeWeight(5);
+	for(var i=0;i<nLines;i++)
+	{
+		stroke(128);
+		strokeWeight(thickness);
+		line(0,i*height/nLines,width,i*height/nLines);
+		line(i*width/nLines,0,i*width/nLines,height);
+		stroke(255);
+		
+	}
+	for(var i=0; i<nLines; i++)
+		for(var j=0; j<nLines;j++)
+		{
+			strokeWeight(1);
+			ellipse(i*width/nLines,j*height/nLines,thickness,thickness);
+		}
+
+}
